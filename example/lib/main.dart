be9b996c08b1a59fe6f48c9ae26a93850f1ac29d
Rohan <rohanpd.work@gmail.com>
2020-03-17 22:17:36 +0530

    done with quotes library

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:quotes/quotes.dart';
-import 'package:quotes/quote_model.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,10 +10,31 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: FlatButton(child: Text("CLICK"),onPressed: (){
-             print(Quotes.getFirst().getAuthorId());
-              print(Quotes.getLast().getAuthorId());
-              print(Quotes.getRandom().getAuthorId());
-              
+          
+              //Getting the First quote that is same everytime
+                print(Quotes.getFirst().getId());
+                print(Quotes.getFirst().getContent());
+                print(Quotes.getFirst().getAuthor());
+                print(Quotes.getFirst().getAuthorId());
+                print(Quotes.getFirst().getTags());
+                print(Quotes.getFirst().getLength());
+                
+               //Getting the Last  quote that is same everytime
+                print(Quotes.getLast().getId());
+                print(Quotes.getLast().getContent());
+                print(Quotes.getLast().getAuthor());
+                print(Quotes.getLast().getAuthorId());
+                print(Quotes.getLast().getTags());
+                print(Quotes.getLast().getLength());
+                          
+              //Getting the random quote
+                print(Quotes.getRandom().getId());
+                print(Quotes.getRandom().getContent());
+                print(Quotes.getRandom().getAuthor());
+                print(Quotes.getRandom().getAuthorId());
+                print(Quotes.getRandom().getTags());
+                print(Quotes.getRandom().getLength());
+             
               
           },),
         ),
